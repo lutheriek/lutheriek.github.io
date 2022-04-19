f1c30d205ef285b252dfe27e1aa1e695bd98186c
lutheriek <97010024+lutheriek@users.noreply.github.com>
2022-04-19 15:09:31 +0700

    Update main.5e0b84bc.chunk.js

--- stake/static/js/main.5e0b84bc.chunk.js
+++ stake/static/js/main.5e0b84bc.chunk.js
@@ -17,7 +17,7 @@ var gasPrice = 40000000000;
 			children:Object(r.jsxs)("div",{className:"row justify-content-between mt-3",
 				children:[Object(r.jsxs)("div",{className:"col-md-12 top-holder",
 					children:[Object(r.jsx)("h1",{className:"page-title",children:"Welcome to Bored Octopus Club Staking Lab"}),
-					Object(r.jsx)("p",{className:"subtitle",children:"Send your Bored Octopus to study and they will earn 0.1 TENTA Token every 2 hours!"}),
+					Object(r.jsx)("p",{className:"subtitle",children:"Send your Bored Octopus to study and they will earn 1 TENTA Token every 2 hours!"}),
 					Object(r.jsx)("p",{className:"primary-text",children:"Stake your Bored Octopus to earn TENTA Token  -  Utility Token for the Bored Octopus Club - You can now mint other project with TENTA Token!"})]}),
 			]})})},
 		o=n(13),p=n(11),c=n.n(p),l=n(17),y=n(109),d=n.n(y),m=n(108),b=n.n(m),f={},
