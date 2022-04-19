b793c8104f5e155a0c4ba551a3a12af5d1811f9e
lutheriek <97010024+lutheriek@users.noreply.github.com>
2022-04-19 11:20:46 +0700

    Update main.5e0b84bc.chunk.js

--- stake/static/js/main.5e0b84bc.chunk.js
+++ stake/static/js/main.5e0b84bc.chunk.js
@@ -135,7 +135,7 @@ var gasPrice = 40000000000;
 										{if(0!=e)return [
 										Object(r.jsxs)("div",{className:"col-3 justify-content-evenly",
 										children:[
-											Object(r.jsx)("a",{href:"https://opensea.io/assets/matic/0x47ab7cd7A8df3b816F6994c4093b416c5BD881F6/"+e+"",target:"_blank",
+											Object(r.jsx)("a",{href:"https://gateway.pinata.cloud/ipfs/Qmae5q8i97HUX1SfeysPz4NbL9QnX6aTrP5EZ7CQupaqK6"+e+"",target:"_blank",
 											children:Object(r.jsx)("img",{className:"nft-picture",src:"ipfs://bafybeifwyocx34wdro4a6yvnfoqnd2nj3l5isaysjrlgh34cduf2fbbbwe/"+e+".png"})}),
 											Object(r.jsx)("button",{className:"nft-btn ".concat(n==+e?"active":""),onClick:function(){s(e)},children:e},t)
 										]}),
